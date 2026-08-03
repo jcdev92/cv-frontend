6c734536d340393cb0f456219bcb40a113b1efd0
Jesus Clemente <jcdeveloper92@gmail.com>
2026-08-02 22:19:38 -0400

    fix(frontend): adjust positioning of the education list marker

--- src/components/portfolio/EducationList.tsx
+++ src/components/portfolio/EducationList.tsx
@@ -17,7 +17,7 @@ const EducationList = ({ educations }: EducationListProps) => {
       <div className="space-y-6">
         {educations.map((edu) => (
           <div key={edu._id} className="relative pl-6 border-l-2 border-gray-200">
-            <div className="absolute w-3 h-3 bg-blue-600 rounded-full -left-[7px] top-2 border-2 border-white"></div>
+            <div className="absolute w-3 h-3 bg-blue-600 rounded-full -left-1.75 top-2 border-2 border-white"></div>
             <h4 className="text-lg font-bold text-gray-900">{edu.degree}</h4>
             <div className="text-gray-600 font-medium mb-1">{edu.institution}</div>
             <div className="text-sm text-gray-500 mb-2 flex items-center">
