8f0988e7907b46b58c0d2105f2caf8c37eb43ae6
Jesus Clemente <jcdeveloper92@gmail.com>
2026-08-07 13:22:18 -0400

    feat: actualizar la configuración de axios para usar la URL de API desde las variables de entorno

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Tu API de backend (Docker)
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 });
 
 // Interceptor para inyectar el token en cada petición automáticamente
